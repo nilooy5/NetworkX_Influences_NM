fa37b86f31f999e89bb7ed656c21eb3c8ec6cb99
Fazal Mahmud Niloy <niloooy@gmail.com>
2022-11-27 05:18:11 +1100

    changes made

--- main.py
+++ main.py
@@ -18,6 +18,13 @@ def most_influential_node(G):
     return degree[0][0]
 
 
+# using networkx pagerank determine top 10 influential nodes
+def top10_influencers(G):
+    pr = nx.pagerank(G)
+    top10 = sorted(pr, key=pr.get, reverse=True)[:10]
+    return top10
+
+
 # read musae_facebook_target.csv in facebook_large folder
 target = pd.read_csv('facebook_large/musae_facebook_target.csv')
 # read musae_facebook_edges.csv
@@ -37,20 +44,35 @@ G = nx.Graph()
 G.add_nodes_from(target['id'])
 # add edges to the graph
 G.add_edges_from(edges.values)
-nx.draw(G, with_labels=True)
-plt.show()
+# nx.draw(G, with_labels=True)
+# plt.show()
 
 # get the most influential node
-most_influential = most_influential_node(G)
-# get the most influential node's feature
-most_influential_feature = features[features['id'] == most_influential].values[0][1:]
-# get the most influential node's label
-most_influential_label = target[target['id'] == most_influential].values[0][1]
+# most_influential = most_influential_node(G)
+# # get the most influential node's feature
+# most_influential_feature = features[features['id'] == most_influential].values[0][1:]
+# # get the most influential node's label
+# most_influential_label = target[target['id'] == most_influential].values[0][1]
+#
+# # print the most influential node's feature
+# print(most_influential_feature)
+# # print the most influential node's label
+# print(most_influential_label)
 
-# print the most influential node's feature
-print(most_influential_feature)
-# print the most influential node's label
-print(most_influential_label)
+# get the top 10 influencers
+top10 = top10_influencers(G)
+# get the top 10 influencers' features
+# top10_features = features[features['id'].isin(top10)].values[:, 1:]
+# get the top 10 influencers' labels
+top10_labels = target[target['id'].isin(top10)].values[:, 1]
 
-# output
+# print the top 10 influencers' features
+# print(top10_features)
+# print the top 10 influencers' labels
+# print(top10_labels)
+
+# target[target['facebook_id'].isin(top10_labels)]['page_name']
+# get page_name from target where facebook_id is top10_labels
+page_names = target[target['facebook_id'].isin(top10_labels)]['page_name']
+print(page_names)
 
